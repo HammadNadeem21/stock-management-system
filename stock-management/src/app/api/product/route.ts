a5544c94bd609a5250992fae113c5037ea56eac8
HammadNadeem21 <nhammad424@gmail.com>
2025-03-01 01:25:16 +0500

    updated

---
+++ stock-management/src/app/api/product/route.ts
@@ -0,0 +1,52 @@
+import { ok } from "assert";
+import { MongoClient } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // Replace the uri string with your connection string.
+  const uri = process.env.MONGODB_URI as string;
+
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = {};
+    const products = await inventory.find(query).toArray();
+
+    return NextResponse.json({success:true, products });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
+
+
+// POST
+export async function POST(request: NextRequest) {
+
+
+    let body = await request.json()
+
+
+    // Replace the uri string with your connection string.
+    const uri = process.env.MONGODB_URI as string;
+  
+    const client = new MongoClient(uri);
+  
+    try {
+      const database = client.db("stock");
+      const inventory = database.collection("inventory");
+  
+      const product = await inventory.insertOne(body);
+  
+      return NextResponse.json({ product, ok:true });
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
--- stock-management/src/components/CurrentStock.tsx
+++ stock-management/src/components/CurrentStock.tsx
@@ -1,12 +1,67 @@
-import React from "react";
-
+"use client";
+import React, { useEffect, useState } from "react";
+interface Product {
+  slug: string;
+  quantity: number;
+  price: number;
+}
+type productProp = Product
 const CurrentStock = () => {
+  const [productForm, setProductForm] = useState<Product>({
+    slug: "",
+    quantity: 0,
+    price: 0
+  });
+  const [products, setProducts] = useState<Product[]>([]);
+  const [alert, setAlert] = useState("");
+
+// fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("http://localhost:3000/api/product");
+      const res = await response.json();
+      setProducts(res.products);
+    };
+    fetchProducts();
+  }, []);
+
+
+  const addProduct = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/product", {
+        method: "POST", //POST request for data sending
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(productForm), //productForm data convert into json string
+      });
+
+      if (response.ok) {
+        console.log("Product add Successfuly");
+        setAlert("Your Product has been added!");
+        setProductForm({ slug: "", quantity: 0, price: 0 })
+      } else {
+        console.log("Error");
+      }
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProductForm({ ...productForm, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
+        <div className="text-green-800 text-center">{alert}</div>
+
       {/* Search Product */}
       <div className="container bg-[#51d0d2] mx-auto mt-10 px-5 py-3 rounded-lg">
         <h1 className="text-3xl font-semibold mb-6">Search a Product</h1>
-        <div className="flex mb-6">
+        <div className="flex mb-6 gap-2">
           <input
             type="text"
             placeholder="Enter a product name"
@@ -28,6 +83,9 @@ const CurrentStock = () => {
               Product Slug
             </label>
             <input
+              value={productForm?.slug || ""}
+              name="slug"
+              onChange={handleChange}
               type="text"
               id="productName"
               className="w-full border border-gray-300 px-4 py-2 rounded-lg"
@@ -39,6 +97,9 @@ const CurrentStock = () => {
               Quantity
             </label>
             <input
+              value={productForm?.quantity || ""}
+              name="quantity"
+              onChange={handleChange}
               type="number"
               id="quantity"
               className="w-full border border-gray-300 px-4 py-2 rounded-lg"
@@ -50,6 +111,9 @@ const CurrentStock = () => {
               Price
             </label>
             <input
+              value={productForm?.price || ""}
+              name="price"
+              onChange={handleChange}
               type="number"
               id="price"
               className="w-full border border-gray-300 px-4 py-2 rounded-lg"
@@ -57,6 +121,7 @@ const CurrentStock = () => {
           </div>
 
           <button
+            onClick={addProduct}
             type="submit"
             className="bg-[#aee45c] hover:bg-[#70b110] transition-all duration-300 text-white px-4 py-2 rounded-xl"
           >
@@ -66,29 +131,30 @@ const CurrentStock = () => {
       </div>
 
       <div className="container bg-[#51d0d2] mx-auto mt-10 mb-5 px-5 py-3 rounded-lg">
-        <h1>Display Current Stock</h1>
+        <h1 className="text-3xl font-semibold mb-6">Display Current Stock</h1>
 
         <table className="table-auto w-full border border-black">
           <thead>
             <tr>
               <th className="border border-black px-4 py-2">Product Name</th>
               <th className="border border-black px-4 py-2">Quantity</th>
-              <th className="border border-black px-4 py-2">Number</th>
+              <th className="border border-black px-4 py-2">Price</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td className="border border-black px-4 py-2">Product Name</td>
-              <td className="border border-black px-4 py-2">Quantity</td>
-              <td className="border border-black px-4 py-2">Number</td>
+            {products.map((item) => {
+              return (
+                <tr key={item.slug}>
+              <td className="border border-black px-4 py-2">{item.slug}</td>
+              <td className="border border-black px-4 py-2">{item.quantity}</td>
+              <td className="border border-black px-4 py-2">Rs {item.price}</td>
             </tr>
+              )
+            })}
+            
 
-            <tr>
-              <td className="border border-black px-4 py-2">Product A</td>
-              <td className="border border-black px-4 py-2">10</td>
-              <td className="border border-black px-4 py-2">$9.00</td>
-            </tr>
+        
           </tbody>
         </table>
       </div>
