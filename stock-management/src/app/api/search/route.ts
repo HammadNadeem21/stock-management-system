1367082bceab571637fab5d3c4cb23176c5de6b1
HammadNadeem21 <nhammad424@gmail.com>
2025-03-02 04:21:07 +0500

    updated

--- stock-management/src/app/api/search/route.ts
+++ stock-management/src/app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { Db, MongoClient } from "mongodb";
+import {  MongoClient } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
