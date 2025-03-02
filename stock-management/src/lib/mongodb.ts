29b8baa63b55144b2d7c5432383b84af7353a21a
HammadNadeem21 <nhammad424@gmail.com>
2025-03-03 02:25:57 +0500

    updated

--- stock-management/src/lib/mongodb.ts
+++ stock-management/src/lib/mongodb.ts
@@ -3,18 +3,13 @@ import { MongoClient, Db } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 const options = {};
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
-}
+const client = new MongoClient(uri, options);
+const clientPromise: Promise<MongoClient> = global._mongoClientPromise || client.connect();
 
-clientPromise = global._mongoClientPromise as Promise<MongoClient>;
+global._mongoClientPromise = clientPromise;
 
 export default clientPromise;
